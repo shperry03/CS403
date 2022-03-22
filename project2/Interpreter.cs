ff29dd1854a0f2931d2a14634f5bff6d14066c36
ebuinevicius <embuinevicius@gmail.com>
2022-03-22 16:51:31 -0500

    new

---
+++ project2/Interpreter.cs
@@ -0,0 +1,4 @@
+namespace project2 {
+
+    class Interpreter : 
+}
---
+++ project2/Lox.cs
@@ -0,0 +1,28 @@
+using System;
+
+
+namespace project2
+{
+
+    class Lox {
+
+        public static void Main(string[] args) {
+
+            if (args.Length > 1) {
+                Console.WriteLine("Usage: jlox [script]");
+                Environment.Exit(64);
+
+            } else if (args.Length == 1) {
+                
+            }
+        }
+
+        private static void runFile(String path) : IOException {
+
+            
+        }
+
+    }
+
+
+}
