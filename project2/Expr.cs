e211410cc6b95bca3f2f876f3e9aeb0e728f932b
shperry <shperry@crimson.ua.edu>
2022-03-23 14:38:07 -0500

    Added generate ast and Expr.cs - Chapter 5 changes

--- project2/Expr.cs
+++ project2/Expr.cs
@@ -1,20 +1,76 @@
 
+
 namespace project2
 {
-    abstract class Expr
+    public abstract class Expr
     {
-        public class Binary : Expr
+        public interface Visitor<T>
         {
-            Expr Left;
-            Token Op;
-            Expr Right;
-            public Binary(Expr left, Token op, Expr right)
+            T visitBinaryExpr(Binary expr);
+            T visitGroupingExpr(Grouping expr);
+            T visitLiteralExpr(Literal expr);
+            T visitUnaryExpr(Unary expr);
+        }
+
+        public class Binary: Expr
+        {
+            Binary(Expr left, Token op, Expr right) {
+                this.left = left;
+                this.op = op;
+                this.right = right;
+            }
+
+            Expr left;
+            Token op;
+            Expr right;
+            public override T Accept<T>(Visitor<T> visitor)
             {
-                this.Left = left;
-                this.Op = op;
-                this.Right = right;
+                return visitor.visitBinaryExpr(this);
             }
         }
 
+        public class Grouping: Expr
+        {
+            Grouping(Expr expression) {
+                this.expression = expression;
+            }
+
+            Expr expression;
+            public override T Accept<T>(Visitor<T> visitor)
+            {
+                return visitor.visitGroupingExpr(this);
+            }
+        }
+
+        public class Literal: Expr
+        {
+            Literal(Object value) {
+                this.value = value;
+            }
+
+            Object value;
+            public override T Accept<T>(Visitor<T> visitor)
+            {
+                return visitor.visitLiteralExpr(this);
+            }
+        }
+
+        public class Unary: Expr
+        {
+            Unary(Token op, Expr right) {
+                this.op = op;
+                this.right = right;
+            }
+
+            Token op;
+            Expr right;
+            public override T Accept<T>(Visitor<T> visitor)
+            {
+                return visitor.visitUnaryExpr(this);
+            }
+        }
+        public abstract T Accept<T>(Visitor<T> visitor);
+
+    
     }
 }
---
+++ project2_tool/GenerateAst.cs
@@ -0,0 +1,102 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+
+
+namespace project2_tool
+{
+    public class GenerateAst
+    {
+        public static void Main(string[] args)
+        {
+            if(args.Length != 1)
+            {
+                Console.WriteLine("Usage: generate_ast <output directory>");
+                Environment.Exit(64);
+            }
+
+            string outputDir = args[0];
+
+            var exprList = new List<string>();
+
+            exprList.Add("Binary   : Expr left, Token operator, Expr right");
+            exprList.Add("Grouping : Expr expression");
+            exprList.Add("Literal  : Object value");
+            exprList.Add("Unary    : Token operator, Expr right");
+
+            defineAst(outputDir, "Expr", exprList);
+        }
+
+        private static void defineAst(string OutputDir, string BaseName, List<string> types)
+        {
+            string Path = OutputDir + "/" + BaseName +".cs";
+            
+            var Code = new List<string>();
+
+            Code.Add("namespace project2");
+            Code.Add("{");
+            Code.Add("    public abstract class" + BaseName);
+            Code.Add("{");
+            
+            defineVisitor(Code, BaseName, types);
+
+            foreach (var type in types){
+                string[] typeParts = type.Split(':');
+                string className = typeParts[0].Trim();
+                string fields = typeParts[1].Trim();
+
+                defineType(Code, BaseName, className, fields);
+            }
+
+            Code.Add("");
+            Code.Add("    abstract <T> T accept(Visitor<T> visitor);");
+
+            Code.Add("}");
+
+            File.WriteAllLines(Path,Code);
+        }
+
+        private static void defineType(List<string> code, string BaseName, string className, string fields)
+        {
+            code.Add("");
+            code.Add("       static class " + className + ": " + BaseName);
+            code.Add("        {");
+            code.Add("            " + className + "(" + fields + ") {");
+            
+            string[] fieldsList = fields.Split(',');
+            foreach(string field in fieldsList)
+            {
+                string name = field.Split(' ')[1];
+                code.Add("                this." + name + " = " + name + ";");
+            }
+            code.Add("            }");
+
+            code.Add("");
+            foreach(string field in fieldsList)
+            {
+                code.Add("          final "+ field +";");
+            }
+
+            code.Add("            public override T Accept<T>(Visitor<T> visitor)");
+            code.Add("            {");
+            code.Add("                return visitor.visit" + className + BaseName + "(this);");
+            code.Add("            }");
+            code.Add("        }");
+        }
+
+        private static void defineVisitor(List<string> code, string BaseName, List<string> types)
+        {
+            code.Add("        public interface Visitor<T>");
+            code.Add("        {");
+
+            foreach(string type in types)
+            {
+                string typeName = type.Split(':')[0].Trim();
+                code.Add("            T visit" + typeName + BaseName + "(" + typeName + " " + BaseName.ToLower() + ");");
+            }
+
+            code.Add("        }");
+        }
+    }
+}
