bedd3027ce6f64b8b09980b80685e0a2cd56edc7
shperry <shperry@crimson.ua.edu>
2022-04-06 15:51:16 -0500

    Fixed function() in parser and wrote TestLox2.txt

--- project2/Parser.cs
+++ project2/Parser.cs
@@ -145,6 +145,7 @@ namespace project2
 
         private Stmt.Function Function(string kind){
             Token name = Consume(TokenType.IDENTIFIER, "Expect " + kind + " name.");
+            Token p = Consume(TokenType.LEFT_PAREN, "Expect '(' before parameter list");
             List<Token> parameters = new List<Token>();
 
             if(!Check(TokenType.RIGHT_PAREN)) {
@@ -153,11 +154,11 @@ namespace project2
                        Error(Peek(), "Can't have more than 255 parameters."); 
                     }
 
-                    parameters.Add(Consume(TokenType.IDENTIFIER, "Expect paramter name."));
+                    parameters.Add(Consume(TokenType.IDENTIFIER, "Expect parameter name."));
                 }while (Match(TokenType.COMMA));
 
             }
-            Consume(TokenType.RIGHT_PAREN, "Expect ')' after paramters.");
+            Consume(TokenType.RIGHT_PAREN, "Expect ')' after parameters.");
             Consume(TokenType.LEFT_BRACE, "Expect '{' before "+kind+ " body.");
             List<Stmt> body = Block();
             return new Stmt.Function(name,parameters,body);
